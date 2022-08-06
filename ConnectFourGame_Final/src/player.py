43df7c8ba029ecbb24ec3e5f1eec84fb44ff881d
Brandon Claspill <100130999+Reddragon300@users.noreply.github.com>
2022-08-06 07:08:38 -0500

    Add files via upload

---
+++ ConnectFourGame_Final/src/player.py
@@ -0,0 +1,22 @@
+'''
+
+@author: Brandon Claspill
+'''
+class Player:
+    def __init__(self, id):
+        self._id = id
+    
+    def get_id(self):
+        return self._id
+
+    def get_color(self):
+        if self._id == 0:
+            return (255,0,0)
+        else:
+            return (0,0,0)
+
+    def get_name(self):
+        if self._id == 0:
+            return "Red"
+        else:
+            return "Black"
